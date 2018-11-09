1b4f6e92af74591b8ea8d23e355c7ca5c29b8a6c
Falk Puschner <Falk.Puschner.gdm@fh-zwickau.de>
2018-11-09 14:06:46 +0100

    Initial commit

---
+++ src/main/java/com/cnfe/quit/App.java
@@ -0,0 +1,13 @@
+package com.cnfe.quit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
