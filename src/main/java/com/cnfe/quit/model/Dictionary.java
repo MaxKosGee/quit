4ab60cccc3ccdf735d12ab94e35069b28ed7d6ae
eclipse20134 <falk.puschner@arcor.de>
2018-11-09 19:52:58 +0100

    change dictionary interface

--- src/main/java/com/cnfe/quit/model/Dictionary.java
+++ src/main/java/com/cnfe/quit/model/Dictionary.java
@@ -1,6 +1,8 @@
 package com.cnfe.quit.model;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 /**
  * This interface is used to declare a basic dictionary.
@@ -12,42 +14,42 @@ public interface Dictionary {
 
 	/**
 	 * Return all languages that will be supported.
-	 * 
+	 *
 	 * @return all languages
 	 */
-	public List<String> getAllLanguages();
+	public List<Locale> getAllLanguages();
 
 	/**
 	 * Translate a word into another language. The source language will be
 	 * automatically detected.
-	 * 
+	 *
 	 * @param word
 	 *            word that should be translated
 	 * @param target
 	 *            target language
-	 * @return translated word
+	 * @return translated words
 	 */
-	public String translate(String word, String target);
+	public List<String> translate(String word, String target);
 
 	/**
 	 * Translate a word into another language.
-	 * 
+	 *
 	 * @param word
 	 *            word that should be translated
 	 * @param source
 	 *            source language
 	 * @param target
 	 *            target target language
-	 * @return translated word
+	 * @return translated words
 	 */
-	public String translate(String word, String source, String target);
+	public List<String> translate(String word, String source, String target);
 
 	/**
 	 * Detach the language of a word.
-	 * 
+	 *
 	 * @param word
 	 *            word to detach the language
-	 * @return detached language
+	 * @return detached language as locale
 	 */
-	public String detachLanguage(String word);
+	public Optional<Locale> detachLanguage(String word);
 }
