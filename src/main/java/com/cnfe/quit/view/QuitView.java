7eea27f6dd11d4ca642d04fae32a335512dc89ab
eclipse20134 <falk.puschner@arcor.de>
2018-11-09 16:34:24 +0100

    add javafx to pom + edit view

--- src/main/java/com/cnfe/quit/view/QuitView.java
+++ src/main/java/com/cnfe/quit/view/QuitView.java
@@ -1,16 +1,25 @@
 package com.cnfe.quit.view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * This class is used to start the main view of the application
+ *
+ * @author eclipse20134 and MaxKosGee
+ * @version 0.1
+ */
 public class QuitView extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		BorderPane root = new BorderPane();
+		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
 		Scene scene = new Scene(root);
+		scene.getStylesheets().add("application.css");
 
 		stage.setScene(scene);
 		stage.show();
