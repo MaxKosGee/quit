c0144bcc71b5b901fba452dbf0b13c61dcc83085
eclipse20134 <falk.puschner@arcor.de>
2018-11-09 16:35:38 +0100

    create notification handler

---
+++ src/main/java/com/cnfe/quit/util/Notification.java
@@ -0,0 +1,84 @@
+package com.cnfe.quit.util;
+
+import javafx.util.Duration;
+import tray.animations.AnimationType;
+import tray.notification.NotificationType;
+import tray.notification.TrayNotification;
+
+/**
+ * Create a notification for the user to tell what happened.
+ *
+ * @author eclipse20134
+ * @version 0.1
+ */
+public class Notification {
+
+	/**
+	 * time for the notification to be visible
+	 */
+	private static final double DELAY = 3;
+
+	/**
+	 * Show an information notification.
+	 *
+	 * @param title
+	 *            main message
+	 * @param message
+	 *            description
+	 */
+	public static void info(String title, String message) {
+		showDialog(title, message, NotificationType.INFORMATION);
+	}
+
+	/**
+	 * Show an error notification.
+	 *
+	 * @param title
+	 *            main message
+	 * @param message
+	 *            description
+	 */
+	public static void error(String title, String message) {
+		showDialog(title, message, NotificationType.ERROR);
+	}
+
+	/**
+	 * Show a notice notification.
+	 *
+	 * @param title
+	 *            main message
+	 * @param message
+	 *            description
+	 */
+	public static void notice(String title, String message) {
+		showDialog(title, message, NotificationType.NOTICE);
+	}
+
+	/**
+	 * Show a success notification.
+	 *
+	 * @param title
+	 *            main message
+	 * @param message
+	 *            description
+	 */
+	public static void success(String title, String message) {
+		showDialog(title, message, NotificationType.SUCCESS);
+	}
+
+	/**
+	 * Create a basic notification.
+	 *
+	 * @param title
+	 *            main message
+	 * @param message
+	 *            description
+	 * @param type
+	 *            notification type
+	 */
+	private static void showDialog(String title, String message, NotificationType type) {
+		TrayNotification notification = new TrayNotification(title, message, type);
+		notification.setAnimationType(AnimationType.POPUP);
+		notification.showAndDismiss(Duration.seconds(DELAY));
+	}
+}
